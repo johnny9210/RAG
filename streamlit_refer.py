fd976647592515917412c69a40740ce92524d180
johnny9210 <33116818+johnny9210@users.noreply.github.com>
2024-01-09 08:50:27 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -85,8 +85,8 @@ def main():
                 st.markdown(response)
                 with st.expander("참고 문서 확인"):
                     st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
-                    st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
-                    st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
+                    # st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
+                    # st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
                     
 
 
